b378ac9c7a8f8dfd121222ecdd00a2b340082f56
Mafym <maksiulek77@gmail.com>
2024-04-15 13:38:59 +0200

    blabla

--- powtorzenie/Program.cs
+++ powtorzenie/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class plikglowny
+{
+    static void Main(string[] args)
+    {
+
+        tabliczka.wyswietl(); 
+
+       
+    }
+}
