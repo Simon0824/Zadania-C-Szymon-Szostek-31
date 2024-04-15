93335cc9d03c0b43876a71e011b3cdbeba3d8edb
Mafym <maksiulek77@gmail.com>
2024-04-15 13:58:33 +0200

    adg

--- powtorzenie/Program.cs
+++ powtorzenie/Program.cs
@@ -1,10 +1,10 @@
-﻿class plikglowny
+﻿using powtorzenie;
+
+class plikglowny
 {
     static void Main(string[] args)
     {
-
+        tablice tabliczka = new tablice();
         tabliczka.wyswietl(); 
-
-       
     }
 }
--- powtorzenie/tablice.cs
+++ powtorzenie/tablice.cs
@@ -8,9 +8,16 @@ namespace powtorzenie
 {
    public class tablice
     {
-        public void wyswietl()
+        public void Tworzenie_tablicy()
         {
-
+            Console.WriteLine("Ile znaków ma mieć tablica?");
+            int dl = int.Parse(Console.ReadLine());
+            int[] ints = new int[dl];
+            for (int i = 0; i < dl; i+)
+            {
+                Console.Write("Podaj kolejny element tablicy: ");
+                ints[i] = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
