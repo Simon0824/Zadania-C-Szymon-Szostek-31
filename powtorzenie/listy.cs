b4412ab5bb17bc124f5d6f1cc46ebe6b1342f960
Simon0824 <szymon.szostek008@gmail.com>
2024-05-13 14:06:13 +0200

    listy
    ddfh

---
+++ powtorzenie/listy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powtorzenie
+{
+    public class Listy
+    {
+
+        public List<int> Stowrzliste()
+        {
+            List<int> lista = new List<int>();
+            string decyzja = "";
+            while(decyzja == "t")
+            {
+                Console.WriteLine("Czy chcesz dodać element");
+                decyzja = Console.ReadLine();
+                if (decyzja == "t")
+                {
+                    Console.Write("Podaj element");
+                    int element = int.Parse(Console.ReadLine());
+                    lista.Add(element);
+                }
+                else
+                {
+                    Console.WriteLine("Podano wszystkie elementy");
+                }
+            }
+
+        }
+
+    }
+}
